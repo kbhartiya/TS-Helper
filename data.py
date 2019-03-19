33a271f7c249639ffcdeeca821b59e3766b6bbfd
kbhartiya <https://github.com/kbhartiya/iris_classifier.git>
2019-03-20 04:37:47 +0530

    Initial commit

---
+++ data.py
@@ -0,0 +1,10 @@
+data = []
+with open('/home/justbj/Projects/Windspeed-forecasting/beresford061203.csv','r') as f:
+	for line in f.readlines()[55:]:
+		#print(type(line))
+		data.append(line)
+	
+with open('./data.csv','w') as fo:
+	for line in data:
+		#print(type(line))
+		fo.write(line)		
